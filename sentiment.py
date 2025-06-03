bffcce1f5cfafe21a4ae70366c037be4eb50ddca
noalpha-left <redwanrafid@gmail.com>
2025-06-03 10:29:29 -0400

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,11 +1,10 @@
 
-
 from textblob import TextBlob
 
 def analyze_sentiment(text_list):
     sentiments = []
     for text in text_list:
         blob = TextBlob(text)
-        score = blob.sentiment.polarity  # range from -1 to 1
+        score = blob.sentiment.polarity
         sentiments.append(score)
     return sentiments
